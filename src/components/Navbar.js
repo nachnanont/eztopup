7eb7cfb9af5da56c5fbe82dd00177032c2e66df1
unknown <nachnanont@gmail.com>
2025-12-06 17:40:13 +0700

    fix:navbar-premium-delete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -81,14 +81,14 @@ function NavbarContent() {
           >
             เติมเกม
           </Link>
-          <Link 
+          {/* <Link 
             href="/?category=premium" 
             className={`transition-colors px-3 py-2 rounded-full ${
                 currentCategory === 'premium' ? 'bg-blue-50 text-blue-600 font-bold' : 'text-slate-500 hover:text-blue-600'
             }`}
           >
             แอปพรีเมียม
-          </Link>
+          </Link> */}
         </div>
 
         {/* Right Side */}
